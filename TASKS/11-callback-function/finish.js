ea8e281b8b54f70f60be63fa143f6293a82e38ad
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-06-18 20:18:31 +0300

    done #11 Task

--- TASKS/11-callback-function/finish.js
+++ TASKS/11-callback-function/finish.js
@@ -4,3 +4,15 @@
  *
  * 2. Используйте стрелочную функцию
  */
+
+const hi = function () {
+    console.log('Привет, мир!');
+}
+setTimeout(hi, 2000);
+
+const hiFun = () => {
+    console.log('Привет, мир!');
+}
+setTimeout(hiFun, 3000);
+
+setTimeout(() => console.log('Привет, мир!'), 5000);
