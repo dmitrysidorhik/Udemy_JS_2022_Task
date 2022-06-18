a8c2c5caa9ed4b3bff65bfe1e7bc62e84996e36c
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-06-18 15:39:04 +0300

    done #6 Task

--- TASKS/06-data-types/finish.js
+++ TASKS/06-data-types/finish.js
@@ -12,3 +12,20 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const stringVar = 'string';
+const numberVar = 23;
+const boolVar = true;
+const nullVar = null;
+const undefinedVar = undefined;
+const objVar = {};
+const arrayVar = [];
+
+console.log(typeof (stringVar));
+console.log(typeof (numberVar));
+console.log(typeof (boolVar));
+console.log(typeof (nullVar));
+console.log(typeof (undefinedVar));
+console.log(typeof (objVar));
+console.log(typeof (arrayVar));
+
