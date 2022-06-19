84f21527193d94333c05fd016a10d839ffbe3463
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-06-19 13:21:18 +0300

    done #19 Task

--- TASKS/19-push-to-array/finish.js
+++ TASKS/19-push-to-array/finish.js
@@ -6,3 +6,11 @@
  *
  * 3. Выведите в консоль длину результирующего массива
  */
+
+const array = [1, null, 'text', true, 23];
+console.log(array);
+array.forEach((element) => console.log(element));
+
+array.push(23, 'hello');
+console.log(array);
+
