9b7fecd42225989ec9422236c1ecd2ae865e5ebb
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-06-19 13:04:58 +0300

    done #18 Task

--- TASKS/18-iterate-over-array/finish.js
+++ TASKS/18-iterate-over-array/finish.js
@@ -5,3 +5,19 @@
  * 2. Используя один из методов массивов, переберите все элементы
  * и выведите каждый элемент в консоль
  */
+ const array = [2, null, 1, '2', true, 'hello', 3, 5, undefined];
+
+// Предпочтительнее 
+array.forEach((element)=>{
+    console.log(element)
+})
+console.log('-----------------');
+
+// Предпочтительнее 
+array.forEach((element)=> console.log(element))
+console.log('-----------------');
+
+// Не рекомендуется
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
