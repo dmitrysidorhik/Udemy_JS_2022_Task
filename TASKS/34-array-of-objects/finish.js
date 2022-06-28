96ed661aafc3e99cc63591f93a11c45a11add14c
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-06-28 22:51:52 +0300

    work #34 Task

--- TASKS/34-array-of-objects/finish.js
+++ TASKS/34-array-of-objects/finish.js
@@ -11,3 +11,19 @@
  *
  * 4. Выведите результирующий массив в консоль
  */
+
+const cars = [
+  { carBrand: "lexus", price: 6000, isAvailableForSale: false },
+  { carBrand: "mazda", price: 8000, isAvailableForSale: true },
+  { carBrand: "audi", price: 7000, isAvailableForSale: true },
+];
+console.log(cars);
+cars.forEach((element) => {
+  console.log(element);
+});
+const newCars = { carBrand: "zonda", price: 40000, isAvailableForSale: true };
+cars.push(newCars);
+console.log(cars);
+cars.forEach((element) => {
+  console.log(element);
+});
