0c97919b49737f5c7f32ea80ca4d6649bcdddec2
Dmitry Sidorchyk <dmitrysidorhik@gmail.com>
2022-07-01 16:11:53 +0300

    done 36 Task

--- TASKS/36-random-numbers/finish.js
+++ TASKS/36-random-numbers/finish.js
@@ -13,8 +13,37 @@
  *  - для генерации случайного числа в заданном диапазоне
  *  - для добавления случайного числа в массив с возвратом измененного массива
  */
-
 const MIN = 1000
 const MAX = 9999
 
+// const MIN = 1
+// const MAX = 10
+
 const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351]
+    // const myNumbers = [1, 2, 7, 8, 3, 2]
+
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const addRandomNumberToArray = (arr, min, max) => {
+    let newRandomNumber
+    const updatedArr = [...arr]
+
+    do {
+        newRandomNumber = randomNumber(min, max)
+        console.log(newRandomNumber)
+    } while (updatedArr.includes(newRandomNumber))
+
+    updatedArr.push(newRandomNumber)
+
+    return updatedArr
+}
+
+
+const updatedArray = addRandomNumberToArray(myNumbers, MIN, MAX)
+
+console.log('UPDATED ARRAY', updatedArray)
+console.log('ORIGINAL ARRAY', myNumbers)
+
+// console.log(myNumbers.includes(2355))
