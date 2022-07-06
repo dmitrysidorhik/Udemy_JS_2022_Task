cc605c29ed71f30299a21fd580f07afe91d17277
Dmitry Sidorchyk <dmitrysidorhik@gmail.com>
2022-07-06 16:19:18 +0300

    done 41 Task

--- TASKS/41-find-single-post/finish.js
+++ TASKS/41-find-single-post/finish.js
@@ -13,10 +13,14 @@
  */
 
 const posts = [
-  { postId: 1355, commentsQuantity: 5 },
-  { postId: 5131, commentsQuantity: 13 },
-  { postId: 6134, commentsQuantity: 2 },
+    { postId: 1355, commentsQuantity: 5 },
+    { postId: 5131, commentsQuantity: 13 },
+    { postId: 6134, commentsQuantity: 2 },
 ]
+
+function findPostById(postId, posts) {
+    return posts.find((post) => post.postId === postId)
+}
 
 console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
 
