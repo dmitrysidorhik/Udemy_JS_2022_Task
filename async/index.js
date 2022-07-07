5698634866bb62e27385e487d30be05fa59dcf08
Dmitry Sidorchyk <dmitrysidorhik@gmail.com>
2022-07-07 16:11:19 +0300

    learn #async/await

--- async/index.js
+++ async/index.js
@@ -1,13 +1,22 @@
-const timerPromise = () =>
-    new Promise((resolve, reject) =>
-        setTimeout(() => resolve(), 2000))
+//В браузере
 
-const asyncFn = async() => {
-    console.log('Timer starts')
-    const startTime = performance.now()
-    await timerPromise()
-    const endTime = performance.now()
-    console.log('Timer ended', endTime - startTime)
+// const getDate = (url) =>
+//     new Promise((resolve, reject) =>
+//         fetch(url)
+//         .then(response => response.json())
+//         .then(json => resolve(json))
+//         .catch(error => reject(error))
+//     )
+
+const getDate = async(url) => {
+    const res = await fetch(url)
+    const json = await res.json()
+    return json
 }
 
-asyncFn()
+// getDate('https://jsonplaceholder.typicode.com/todos/23')
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error.message))
+
+const url = 'https://jsonplaceholder.typicode.com/todos/23'
+const data = await getDate(url)
