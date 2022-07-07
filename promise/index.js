5d9c338949d1f10b615a1fd186d349a99f2f5323
Dmitry Sidorchyk <dmitrysidorhik@gmail.com>
2022-07-07 13:20:18 +0300

    learn #fetch in promise

--- promise/index.js
+++ promise/index.js
@@ -1,10 +1,12 @@
 //В браузере
-fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then(response => response.json())
-    // .then(response => {
-    //     console.log(response)
-    //     return response.json()
-    // })
-    .then(json => console.log(json))
-    // .catch(error => console.error(error))
+const getDate = (url) =>
+    new Promise((resolve, reject) =>
+        fetch(url)
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(error => reject(error))
+    )
+
+getDate('https://jsonplaceholder.typicode.com/todos/23')
+    .then(data => console.log(data))
     .catch(error => console.log(error.message))
