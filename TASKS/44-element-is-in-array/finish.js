6063b023de707925bdcc555a36a009d244e17d29
Dmitry Sidorchyk <dmitrysidorhik@gmail.com>
2022-07-08 09:56:40 +0300

    done 44 Task

--- TASKS/44-element-is-in-array/finish.js
+++ TASKS/44-element-is-in-array/finish.js
@@ -12,3 +12,7 @@ const transports = ['Bus', 'Car', 'Bicycle', 'Airplane']
 console.log(isElementInArray(transports, 'Bus')) // true
 console.log(isElementInArray(transports, 'Phone')) // false
 console.log(isElementInArray(transports, 'Airplane')) // true
+
+function isElementInArray(inputArray, searchElement) {
+    return inputArray.includes(searchElement)
+}
