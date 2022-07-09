0f5f1f8062144b10532bbb531091a7ba6938677f
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-07-09 17:51:34 +0300

    done 51 Task

--- TASKS/51-rest-and-spread-operators/finish.js
+++ TASKS/51-rest-and-spread-operators/finish.js
@@ -13,21 +13,42 @@
  * так и оператор "spread".
  */
 
-const scores1 = [0, 1.5, 2.5, 3.7]
-const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
-const scores3 = [1.3, 2.5, 1.9]
-const scores4 = ['abc', 1.3, true, 2.5, 1.9]
+function meanScore(...numders) {
+  if (numders.some((num) => typeof num !== "number")) {
+    console.error("Все аргументы в вызове функции должны быть числами!");
+    // чтобы небыло undefine можно сгенерировать ошибку
+    // throw new Error ('Все аргументы в вызове функции должны быть числами!') //сгенирировали ошибку
+    return;
+  }
+  /**
+   * num1 / numbers.length + num2 / numbers.length + ... + num10 / numbers.length
+   *  эквивалентно
+   * (num1 + num2 + ... + num10) / numbers.length
+   */
+  return numders
+    .reduce((mean, num) => mean + num / numders.length, 0)
+    .toFixed(2); // округление
+}
+const scores1 = [0, 1.5, 2.5, 3.7];
+const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
+const scores3 = [1.3, 2.5, 1.9];
+const scores4 = ["abc", 1.3, true, 2.5, 1.9];
 
-console.log(meanScore(/* все элементы из массива "scores1" */))
+console.log(meanScore(...scores1));
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+console.log(meanScore(...scores1, ...scores2));
 // 2.8
 
-console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
-)
+console.log(meanScore(...scores1, ...scores2, ...scores3));
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */))
+console.log(meanScore(...scores4));
 // Все аргументы в вызове функции должны быть числами!
+
+const myFn = (a, ...b) => {
+  console.log(a);
+  console.log(b);
+};
+
+myFn(3, 5, 8, 4, 1);
